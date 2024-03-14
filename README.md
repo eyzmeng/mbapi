b241fe9ef9027dd715a07e55f5916b4fd9e1fefb
Ethan Meng <ethan@rapidcow.org>
2024-03-14 11:55:00 +0800

    Switch voices

--- README.md
+++ README.md
@@ -1,17 +1,69 @@
 # mbapi
 
-installation:
+easy installation:
 
     pip install git+https://github.com/eyzmeng/mbapi.git
+    curl -O https://raw.githubusercontent.com/eyzmeng/mbapi/main/hello.py
 
 pip is whatever ur python package manager is called
 (pip3, python3 -m pip, etc...)
 
+installation in case i feel bad for leaving any trace
+on ur computer:
+
+    git clone https://github.com/eyzmeng/mbapi.git
+    cd mbapi
+    python -m venv venv
+
+    either one of:
+      source  venv/bin/activate  [*nix shell]
+      venv\Scripts\activate.bat  [Windows cmd.exe]
+      venv\Scripts\Activate.ps1  [Windows Powershell]
+
+    pip install -e .
+
+    [[then i can do rm -rf mbapi or equivalent when i'm done...]]
+
 ## test your stuff!
 
-run hello.py! you should see something like this:
+run hello.py!
 
-    hi! i am Ethan (or Yizheng) :D
-    my chinese name is 蒙以正
-    and my account was signed up by my school...
-    with my e-mail eyzmeng@gmail.com on Jun 06, 2023!
+it'll prompt you for a token, and that can be obtained by
+opening your browser dev tools, hunt down any request with
+the `_managebac_session` token (it's often easier to refresh
+the whole page with the tab open), and YEET it out of the
+browser - okay you get what i mean
+
+you can choose between a friendly voice, like this!
+
+    you are Ethan (or Yizheng) Meng!
+    your chinese name is 蒙以正
+    and your account was signed up by the school...
+    with your e-mail eyzmeng@gmail.com on Jun 06, 2023!
+
+or a boring monotone voice, like this...
+
+    You have been authenticated as Ethan Meng, aka 蒙以正.
+    Your account was registered with the e-mail eyzmeng@gmail.com
+    on Jun 06, 2023 at 08:06 AM in your machine time (offset +0800).
+
+(which is the default, sadly... the world we live in...)
+
+but you can choose! and that's all that matters :)
+
+## IMPORTANT: SECURITY STUFF
+
+remember to keep the token SECRET, as ANYONE in this world
+with that token can access your account. luckily the token
+you put in is only ever written to a file named `.session`,
+so just delete the file and you would've effectively revoked
+all access to your account :)
+
+you'll also see a warning message if you run the code too,
+which i'll put up here tooooo...
+
+    WARNING: your refreshed session key (starts with [...])
+    has been saved to a hidden file here named '.session'.
+
+    If you wish to no longer use this program, you should
+    purge this file to revoke my access to your account.
--- hello.py
+++ hello.py
@@ -1,19 +1,68 @@
 from datetime import datetime
 from rdks.mbapi import StudentAPI
 
-with StudentAPI('saie.managebac.cn', 443) as client:
-    try:
-        client.load_session('.session')
-    except FileNotFoundError:
-        client.token = input('Input token: ')
-    my = client.whoami()
-    client.save_session('.session')
-    print('hi! i am {my[user_nickname]} '
-          '(or {my[user_first_name]}) :D'
+def main():
+    with StudentAPI('saie.managebac.cn', 443) as client:
+        try:
+            client.load_session('.session')
+        except FileNotFoundError:
+            client.token = input('Input token: ')
+        my = client.whoami()
+        session = client.save_session('.session')
+        r = input("HTTP request was successful! would you "
+                  "like me to print out\nyour info in a IN-formal tone "
+                  "or no? (y/[n]) ")
+        if r == 'y':
+            print('yay!!!! XD')
+            print_friendly(my)
+        else:
+            print()
+            print('Okay... as you wish.')
+            print()
+            print_formal(my)
+
+        print(end='\n\033[1;31m')
+        print("WARNING: your refreshed session key "
+              "(starts with {0})"
+              .format(session['secret'][:16]))
+        print("has been saved to a hidden file here named '.session'.")
+        print()
+        print("If you wish to no longer use this program, you should")
+        print("purge this file to revoke my access to your account.",
+              end='\n\033[0m')
+
+def print_friendly(my):
+    print('so you are {my[user_nickname]} '
+          '(or {my[user_first_name]}) {my[user_last_name]}!'
           .format(my=my))
-    print('my chinese name is {my[user_second_name]}'
+    print('your chinese name is {my[user_second_name]}'
           .format(my=my))
     signup = datetime.fromtimestamp(my['user_created_at'])
-    print('and my account was signed up by my school...')
-    print('with my e-mail {my[user_email]} on '
-          '{:%b %d, %Y}!'.format(signup, my=my))
+    print('and your account was signed up by the school...')
+    print('with your e-mail {my[user_email]} on '
+          '{0:%b %d, %Y}!'.format(signup, my=my))
+    if my['user_email'] != 'eyzmeng@gmail.com':
+        print('cool! pleasure meeting you {}! :D'
+              .format(my['user_nickname']))
+    else:
+        print('[[yay! my part is done :D now you can paste '
+              'those guys into the README ^v^]]')
+
+def print_formal(user):
+    print('You have been authenticated as '
+          '{user[user_nickname]} {user[user_last_name]}, aka '
+          '{user[user_second_name]}.'
+          .format(user=user))
+    signup = datetime.fromtimestamp(user['user_created_at']).astimezone()
+    print('Your account was registered with the e-mail '
+          '{user[user_email]}\non {0:%b %d, %Y} at {0:%I:%m %p} '
+          'in your machine time (offset {0:%z}).'
+          .format(signup, user=user))
+    if user['user_email'] != 'eyzmeng@gmail.com':
+        print('Thank you for your cooperation, {0}. :)'
+              .format(user['user_nickname']))
+    else:
+        print("[[Wait... why didn't YOU respond with yes? >:/]]")
+
+if __name__ == '__main__':
+    main()
